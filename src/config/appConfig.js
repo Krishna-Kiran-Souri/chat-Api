7c9ad39a07c6ee20ea09d62a70f0d7bfdfc63cd2
Krishna-Kiran-Souri <73020912+Krishna-Kiran-Souri@users.noreply.github.com>
2020-11-22 19:44:27 +0530

    added mongodb

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -4,7 +4,7 @@ appConfig.allowedCrossOrgin = "";
 appConfig.env = "Dev";
 appConfig.db = {
   uri:
-    "mongodb+srv://krishnakiransouri:<Krish@94>@cluster0.0wnjz.mongodb.net/<chatApplication>?retryWrites=true&w=majority"
+    "mongodb://admin:1234@chatcluster-shard-00-00.3aixo.mongodb.net:27017,chatcluster-shard-00-01.3aixo.mongodb.net:27017,chatcluster-shard-00-02.3aixo.mongodb.net:27017/chatdb?ssl=true&replicaSet=atlas-iozn5e-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
 appConfig.apiVersion = "/api/v1";
 module.exports = {
